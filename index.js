aaf4250609047ac32a2de805749a015e46c17912
jorgepadilla425 <157733573+jorgepad425@users.noreply.github.com>
2025-02-15 15:13:06 -0500

    Improved index.js formatting

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 function addBook(){
-    if(document.getElementById("titleText").value == "" || 
-    document.getElementById("genreText").value == "") 
-    // Change this to a text warning under the input and not an obstructive window alert.
-        window.alert("Missing title/genre!");
-    else {
-    document.getElementById("titleText").value = "";
-    document.getElementById("genreText").value = "";
+    const text = document.getElementById("titleText");
+    const genre = document.getElementById("genreText");
+    let acceptable = true;
+
+    if(text.value == ""){
+        window.alert("Missing title!");
+        acceptable = false;
     }
+    if(genre.value == "") {
+        window.alert("Missing genre!");
+        acceptable = false;
+    }
+    // Change this to a text warning under the input and not an obstructive window alert.
+    if(acceptable == true){
+    text.value = "";
+    genre.value = "";
+    }
+    acceptable = true;
 }
