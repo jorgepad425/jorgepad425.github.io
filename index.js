07fbc74cda8d2a616289b169db39a546358b33f7
jorgepadilla425 <157733573+jorgepad425@users.noreply.github.com>
2025-02-15 15:02:48 -0500

    Added an incomplete function

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-swalala
+function addBook(){
+    if(document.getElementById("titleText").value == "" || 
+    document.getElementById("genreText").value == "") 
+    // Change this to a text warning under the input and not an obstructive window alert.
+        window.alert("Missing title/genre!");
+    else {
+    document.getElementById("titleText").value = "";
+    document.getElementById("genreText").value = "";
+    }
+}
