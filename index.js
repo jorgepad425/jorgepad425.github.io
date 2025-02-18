252abcdfd20aac5ea4163012f4df04e8784c9d32
jorgepadilla425 <157733573+jorgepad425@users.noreply.github.com>
2025-02-18 12:56:33 -0500

    Added array for easier list management

--- index.js
+++ index.js
@@ -1,6 +1,51 @@
+/*
+https://stackoverflow.com/questions/11128700/create-a-ul-and-fill-it-based-on-a-passed-array
+    Used to create booklist as an array rather than directly manipulating the HTML, passing
+values as elements and not arrays.
+*/
+
+var bookarray = [
+    ['Placeholder Book', 'Placeholder Genre'],
+    ['Great Gatsby', 'Tragedy']
+];
+
+(function () {
+    refreshList();
+})();
+
+function getEle(s){
+    const element = document.getElementById(s);
+    return element;
+}
+
+function refreshList(){
+    while (getEle("list").firstChild) {
+        getEle("list").removeChild(getEle("list").firstChild);
+        }
+
+    for(let i = 0; i<bookarray.length; i++){
+        let entry = document.createElement("li");
+        entry.innerHTML = '<div class="book"><li><div class="booktext"><h3>' + bookarray[i][0] + '</h3><h5>' + bookarray[i][1]+ '</h5></div></li></div>';
+        getEle("list").appendChild(entry); 
+    }
+}
+
+/*
+      <div class="book"><li>
+        <div class="booktext">
+        <h3>Book 1</h3>
+        <h5>Thriller</h5>
+      </div></li></div>
+      <div class="book"><li>
+        <div class="booktext">
+        <h3>Book 2</h3>
+        <h5>Horror</h5>
+      </div></li></div>
+*/
+
 function addBook(){
-    const text = document.getElementById("titleText");
-    const genre = document.getElementById("genreText");
+    const text = getEle("titleText");
+    const genre = getEle("genreText");
 
     const mtext = document.getElementById("mtitle");
     const mgen = document.getElementById("mgenre");
@@ -18,10 +63,15 @@ function addBook(){
     }
 
     if(acceptable == true){
-    mtext.setAttribute("hidden", true);
-    mgen.setAttribute("hidden", true);
-    text.value = "";
-    genre.value = "";
+        mtext.setAttribute("hidden", true);
+        mgen.setAttribute("hidden", true);
+
+        let temp = [text.value, genre.value];
+        bookarray.push(temp);
+
+        text.value = "";
+        genre.value = "";
     }
     acceptable = true;
+    refreshList();
 }
